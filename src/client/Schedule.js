20689b99e70aab5fec9d7bf3a429d8a3325ba0ca
MiishaG <babusha952@gmail.com>
2020-01-17 04:26:40 +0300

    =first commit

---
+++ src/client/Schedule.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Schedule extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Schedule</h1>
+      </div>
+    );
+  }
+}
+
+export default Schedule;
