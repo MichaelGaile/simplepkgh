308db7df6c320e247e923efb3ca412d3714a34c8
Michael <babusha9522gmail.com>
2020-04-11 22:43:45 +0300

    Its work! Create route from schedule

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -1,19 +1,35 @@
 const express = require('express');
-const getPkgh = require('../src/GetPkgh.js');
+const { MongoClient } = require('mongodb');
 
 const router = express.Router();
 
-const pkgh = new getPkgh();
+const { db } = require('../config.js');
 
 router.get('/schedule', (req, res) => {
-  pkgh.getSchedule((r) => {
-    res.send(r);
+  MongoClient.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) throw err;
+    client.db('simplepkgh').collection('schedule').find({}, {
+      projection: {
+        _id: 0,
+        table: 0,
+        replace: 0,
+      },
+    }).toArray((e, data) => {
+      if (e) throw e;
+      res.send(data);
+    });
   });
 });
 router.get('/schedule/:id', (req, res) => {
-  pkgh.getScheduleGroup(req.params.id).then((r) => {
-    pkgh.getSchedule((r) => {
-      res.send(r);
+  MongoClient.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    if (err) throw err;
+    client.db('simplepkgh').collection('schedule').findOne({ id: req.params.id }, {
+      projection: {
+        _id: 0,
+      },
+    }, (e, data) => {
+      if (e) throw e;
+      res.send(data);
     });
   });
 });
